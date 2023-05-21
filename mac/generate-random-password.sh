e752b59295e6f80e0731014a6b6716275dc67af9
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-21 18:18:46 +0100

    Update generate-random-password.sh

--- mac/generate-random-password.sh
+++ mac/generate-random-password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Generate a random password
-RANDOM_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%^&*()_-+=' | fold -w 16 | head -n 1)
+RANDOM_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()_-+=' | fold -w 16 | head -n 1)
 
 echo "Random MySQL password: $RANDOM_PASSWORD"
