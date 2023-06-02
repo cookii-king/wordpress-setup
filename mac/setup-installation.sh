b129795e57400f010873b88896da043abd33b9c2
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-06-02 16:32:23 +0100

    Update setup-installation.sh

--- mac/setup-installation.sh
+++ mac/setup-installation.sh
@@ -32,7 +32,7 @@ fi
 if [ ! -z "$4" ]; then
   BACKUP_SERVER="$4"
 else
-  read -p "Enter backup server username: " BACKUP_SERVER
+  read -p "Enter backup server username (leave empty if not using a backup server): " BACKUP_SERVER
 fi
 
 sudo apt update -y
@@ -91,62 +91,10 @@ sudo systemctl restart nginx
 
 sudo chown -R www-data:www-data /var/www
 
-# MYSQL_FILE="${SYSTEM_PATH}backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").sql"
-# LOG_FILE="${SYSTEM_PATH}suretide.log"
-
-# # Calculate the length of the LOG_ENTRY_DATE_TIME text
-# LOG_ENTRY_DATE_TIME="$(date +"%Y-%m-%d %H:%M:%S")"
-
-# # Dump the MySQL database and save it to a file
-# /usr/bin/mysqldump -u$MYSQL_USER $MYSQL_DATABASE > "$MYSQL_FILE"
-# # Upload the MySQL backup file to the backup server using SFTP
-# echo -e "put $MYSQL_FILE\nexit" | /usr/bin/sftp -o StrictHostKeyChecking=no -i "${SYSTEM_PATH}backup.pem" $BACKUP_SERVER >> "$LOG_FILE"
-
-# # Create a tarball of the WordPress directory and save it to a file
-# WORDPRESS_DIRECTORY_TAR_FILE="${SYSTEM_PATH}wordpress_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").tar.gz"
-# WORDPRESS_DIRECTORY="/var/www/html/wordpress"
-# tar -czf "$WORDPRESS_DIRECTORY_TAR_FILE" "$WORDPRESS_DIRECTORY" >> "$LOG_FILE"
-# # Upload the WordPress backup file to the backup server using SFTP
-# echo -e "put $WORDPRESS_DIRECTORY_TAR_FILE\nexit" | /usr/bin/sftp -o StrictHostKeyChecking=no -i "${SYSTEM_PATH}backup.pem" $BACKUP_SERVER >> "$LOG_FILE"
-
-# # Create a tarball of the Nginx directory and save it to a file
-# NGINX_DIRECTORY_TAR_FILE="${SYSTEM_PATH}nginx_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").tar.gz"
-# NGINX_DIRECTORY="/etc/nginx"
-# tar -czf "$NGINX_DIRECTORY_TAR_FILE" "$NGINX_DIRECTORY" >> "$LOG_FILE"
-# # Upload the Nginx backup file to the backup server using SFTP
-# echo -e "put $NGINX_DIRECTORY_TAR_FILE\nexit" | /usr/bin/sftp -o StrictHostKeyChecking=no -i "${SYSTEM_PATH}backup.pem" $BACKUP_SERVER >> "$LOG_FILE"
-
-# # Print the MySQL backup file path
-# echo "$MYSQL_FILE"
-# # Print the log file content
-# cat "$LOG_FILE"
-
-# # Remove the backup files from the local system
-# rm $MYSQL_FILE
-# rm $WORDPRESS_DIRECTORY_TAR_FILE
-# rm $NGINX_DIRECTORY_TAR_FILE
-
-# # Check if the home and site URLs in the database match the current server URL
-# # If not, update the URLs in the database
-# OUTPUT=$(mysql -u$MYSQL_USER -e "use $MYSQL_DATABASE;
-# SELECT option_name, option_value FROM wp_options WHERE option_name IN ('siteurl', 'home');")
-# HOME_URL=$(echo "$OUTPUT" | grep "home" | awk '{print $2}')
-# SITE_URL=$(echo "$OUTPUT" | grep "siteurl" | awk '{print $2}')
-# CURRENT_URL="http://$(curl ifconfig.me)"
-
-# if [ "$HOME_URL" != "$CURRENT_URL" ] && [ "$SITE_URL" != "$CURRENT_URL" ]; then
-#     mysql -u$MYSQL_USER -e "use $MYSQL_DATABASE;
-#     UPDATE wp_options SET option_value = '$CURRENT_URL' WHERE option_name = 'siteurl';
-#     UPDATE wp_options SET option_value = '$CURRENT_URL' WHERE option_name = 'home';"
-#     echo "🔄 Updated home and siteurl values in the database." >> "$LOG_FILE"
-# else
-#     echo "ℹ️ Current homeurl is: $HOME_URL" >> "$LOG_FILE"
-#     echo "ℹ️ Current siteurl is: $SITE_URL" >> "$LOG_FILE"
-# fi
-
-# # Update the cron job to run this script every minute
-# (crontab -l | grep -v "${SYSTEM_PATH}backup.sh"; echo " * * * * * /usr/bin/bash ${SYSTEM_PATH}backup.sh $MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD $BACKUP_SERVER") | sort -u | crontab -
+if [ ! -z "$BACKUP_SERVER" ]; then
+  # Add your backup script here
+  echo "Backup server is set up."
+fi
 
 echo "done ✅ ∙ to get rid of error just setup your wordpres and update the backup script to your liking..."
 echo "go to http://$(curl ifconfig.me) to see finish setting up your wordpress website. 😁"
-
