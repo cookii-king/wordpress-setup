de57084dc72134c050b9442e5ae94c2aa8c1ac27
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-06-03 14:13:17 +0100

    Update setup-installation.sh

--- mac/setup-installation.sh
+++ mac/setup-installation.sh
@@ -5,35 +5,63 @@ BACKUP_SERVER=""
 MYSQL_DATABASE=""
 MYSQL_USER=""
 MYSQL_PASSWORD=""
-# SYSTEM_PATH="/home/ubuntu/system/"
+DOMAIN_NAME=""
 
-# sudo chmod 400 "${SYSTEM_PATH}backup.pem"
-
-# Check if command line arguments are provided
+# Ask for the domain name first
 if [ ! -z "$1" ]; then
-  MYSQL_DATABASE="$1"
+  DOMAIN_NAME="$1"
 else
-  read -p "Enter database name: " MYSQL_DATABASE
+  read -p "Enter domain name e.g. pagebunnii.com (leave empty if not using domain name): " DOMAIN_NAME
 fi
 
+# Ask if the user wants to use the domain name for the MYSQL_DATABASE and MYSQL_USER values
+if [ ! -z "$DOMAIN_NAME" ]; then
+  read -p "Do you want to use your domain name for the database name and user? (y/n): " USE_DOMAIN_NAME
+  if [ "$USE_DOMAIN_NAME" == "y" ]; then
+    MYSQL_DATABASE=$(echo "$DOMAIN_NAME" | sed 's/https\?:\/\///' | sed 's/\..*//')
+    MYSQL_USER="$MYSQL_DATABASE-user"
+  fi
+fi
+
+# Check if command line arguments are provided for the other values
 if [ ! -z "$2" ]; then
-  MYSQL_USER="$2"
+  MYSQL_DATABASE="$2"
 else
-  read -p "Enter database user: " MYSQL_USER
+  if [ -z "$MYSQL_DATABASE" ]; then
+    read -p "Enter database name: " MYSQL_DATABASE
+  fi
 fi
 
 if [ ! -z "$3" ]; then
-  MYSQL_PASSWORD="$3"
+  MYSQL_USER="$3"
 else
-  read -sp "Enter database password: " MYSQL_PASSWORD
-  echo ""
+  if [ -z "$MYSQL_USER" ]; then
+    read -p "Enter database user: " MYSQL_USER
+  fi
 fi
 
 if [ ! -z "$4" ]; then
-  BACKUP_SERVER="$4"
+  MYSQL_PASSWORD="$4"
+else
+  read -p "Do you want to use your own password or generate a new one? (own/generate): " PASSWORD_CHOICE
+  if [ "$PASSWORD_CHOICE" == "own" ]; then
+    read -sp "Enter database password: " MYSQL_PASSWORD
+    echo ""
+  else
+    # Generate a random password
+    MYSQL_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()_-+=' | fold -w 16 | head -n 1)
+    echo "Generated MySQL password: $MYSQL_PASSWORD"
+  fi
+fi
+
+if [ ! -z "$5" ]; then
+  BACKUP_SERVER="$5"
 else
   read -p "Enter backup server username (leave empty if not using a backup server): " BACKUP_SERVER
 fi
+
+# Get the latest PHP version number
+PHP_VERSION=$(sudo apt-cache search php | grep -oP 'php\d\.\d' | sort -V | tail -n 1)
 
 sudo apt update -y
 sudo apt install nginx -y
@@ -41,9 +69,9 @@ sudo apt install software-properties-common -y
 sudo apt install mysql-server -y
 sudo apt install mysql-client -y
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt install php8.1 -y
-sudo apt install php8.1-fpm -y
-sudo apt install php8.1-mysql -y
+sudo apt install "${PHP_VERSION}" -y
+sudo apt install "${PHP_VERSION}-fpm" -y
+sudo apt install "${PHP_VERSION}-mysql" -y
 sudo systemctl status nginx
 sudo wget -O /var/www/html/latest.tar.gz https://wordpress.org/latest.tar.gz
 sudo tar -xzf /var/www/html/latest.tar.gz -C /var/www/html/
@@ -55,27 +83,43 @@ sudo sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '$M
 sudo sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER' );/g" /var/www/html/wordpress/wp-config.php
 sudo sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '$MYSQL_PASSWORD' );/g" /var/www/html/wordpress/wp-config.php
 
-config_file="/var/www/html/wordpress/wp-config.php"
-
-# Download the salts and store them in a variable
-SALT=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
-
-# Define the search string
-STRING='put your unique phrase here'
-
-# Replace the placeholders in the wp-config.php file
-sed -i "/$STRING/ { N; d }" $config_file
-printf '%s\n' "/**#@-/i" "$SALT" "." "w" | ed -s $config_file
+# Check if the domain name is provided
+if [ -z "$DOMAIN_NAME" ]; then
+  DOMAIN_NAME="localhost"
+  REDIRECT_SERVER=""
+else
+  REDIRECT_SERVER="server {
+    listen 80;
+    server_name $(curl ifconfig.me);
+    return 301 http://${DOMAIN_NAME};
+  }"
+fi
 
 # Replace the document root in the Nginx configuration file
-sudo sed -i "s#root /var/www/html;#root /var/www/html/wordpress;#g" /etc/nginx/sites-available/default
-sudo sed -i "s#index index.html index.htm index.nginx-debian.html;#index index.php index.html index.htm index.nginx-debian.html;#g" /etc/nginx/sites-available/default
-sudo sed -i "s/server_name _;/server_name localhost;/g" /etc/nginx/sites-available/default
-sudo sed -i 's#try_files $uri $uri/ =404;#try_files $uri $uri/ /index.php?$args;#g' /etc/nginx/sites-available/default
+sudo bash -c "cat > /etc/nginx/sites-available/default" << EOL
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
 
-sudo sed -i '60s/php7.4-fpm.sock/php8.1-fpm.sock/' /etc/nginx/sites-available/default
+  root /var/www/html/wordpress;
 
-sudo sed -i -e '56, 61 s/#//' -e '63 s/#//' /etc/nginx/sites-available/default
+  index index.php index.html index.htm index.nginx-debian.html;
+
+  server_name ${DOMAIN_NAME} www.${DOMAIN_NAME};
+
+  location / {
+    try_files \$uri \$uri/ /index.php?\$args;
+  }
+
+  location ~ \.php$ {
+    include snippets/fastcgi-php.conf;
+
+    fastcgi_pass unix:/var/run/php/${PHP_VERSION}-fpm.sock;
+  }
+}
+
+${REDIRECT_SERVER}
+EOL
 
 # Create the database, user, and grant privileges
 sudo mysql <<EOF
@@ -86,7 +130,6 @@ GRANT SELECT, SHOW VIEW, RELOAD, REPLICATION CLIENT, LOCK TABLES, PROCESS ON *.*
 FLUSH PRIVILEGES;
 EOF
 
-
 sudo systemctl restart nginx
 
 sudo chown -R www-data:www-data /var/www
@@ -96,5 +139,5 @@ if [ ! -z "$BACKUP_SERVER" ]; then
   echo "Backup server is set up."
 fi
 
-echo "done ✅ ∙ to get rid of error just setup your wordpres and update the backup script to your liking..."
-echo "go to http://$(curl ifconfig.me) to see finish setting up your wordpress website. 😁"
+echo "done ✅ ∙ to get rid of error just setup your WordPress and update the backup script to your liking..."
+echo "Go to http://${DOMAIN_NAME:-$(curl ifconfig.me)} to finish setting up your WordPress website. 😁"
